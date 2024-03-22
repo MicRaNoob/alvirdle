b45fa4d7d5bac83825cbdfc88721446084e54054
MicRa <78746681+MicRaNoob@users.noreply.github.com>
2024-03-22 08:13:40 -0400

    Delete 3.js

--- 3.js
+++
@@ -1,9 +0,0 @@
-custom = ["BUS","GYM","INK","MAP","ART","TRY","JOB"];
-
-beginner = ["BUS","GYM","INK","MAP","ART","TRY","JOB"];
-
-intermediate = beginner.concat(["CTE","GEO","SAT","AHS","ACT","VET","ASK","LOW"]);
-
-advanced = intermediate.concat(["PEP","HOW","WHY","SIT","NET","RUN","JOG"]);
-
-fullList = advanced.concat(["COW","SAU","MRS","FOX","TIP","USE","PEN","ASL"]);
